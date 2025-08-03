b099e24545a8bdbf912928e6d5ba839cf3ab5e30
karanvanzwala <karan.vanzwala@gmail.com>
2025-08-03 15:41:55 +0530

    add env file

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -2,8 +2,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://saleor.kombee.co.in/graphql/',
+  uri: process.env.NEXT_PUBLIC_BASE_URL || '',
   cache: new InMemoryCache(),
 });
 
 export default client;
+    
